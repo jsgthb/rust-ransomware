5fa120a00c3cd8b4c1c7c19c1477c046b269ca23
jsgthb <jeff.schaack@gmail.com>
2024-03-26 19:13:35 +0100

    Added confirmation before encryption

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use inquire::Confirm;
+
 mod files;
 
 fn main() {
-    match files::get_filepaths_in_cwd() {
-        Ok(result) => println!("{:?}", result),
-        Err(e) => println!("{:?}", e)
-    }
+    // Get confirmation from user
+    let confirmation = Confirm::new("Are you sure you want to encrypt the files in the current directory?")
+        .with_default(false)
+        .with_help_message("You are responsible for your own actions")
+        .prompt()
+        .expect("Error with questionaire");
+
+    // Exit if confirmation is false
+    if confirmation == false {
+        println!("Exiting program");
+        std::process::exit(0)
+    } 
 }
