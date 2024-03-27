3e26bbacbf7200ef96e596d322bfee349bcdab5e
jsgthb <jeff.schaack@gmail.com>
2024-03-27 18:37:19 +0100

    Fixed encryption

--- src/files.rs
+++ src/files.rs
@@ -1,4 +1,4 @@
-use std::{fs::{self, File}, io::{BufReader, Error, Read, Write}};
+use std::{fs::{self, File}, io::{Error, Write}};
 
 pub fn get_filepaths_in_cwd() -> Result<Vec<String>, Error> {
     // Create empty vector
@@ -7,34 +7,24 @@ pub fn get_filepaths_in_cwd() -> Result<Vec<String>, Error> {
     let dir = fs::read_dir("./").expect("Failed to read directory");
        
     for entry in dir {
-        let path = entry.expect("Could not read path").path();
+        let entry_unwrapped = entry.expect("Could not read path");
+        let filename = entry_unwrapped.file_name().into_string().expect("Could not convert string");
+        let path = entry_unwrapped.path();
         if path.is_file() {
-            directories.push(path.into_os_string().into_string().expect("Could not convert PathBuf to String"))
+            directories.push(filename)
         }
     }      
     return Ok(directories);
 }
 
 pub fn read_file_bytes(filepath: &str) -> Result<Vec<u8>, Error> {
-    let file = File::open(filepath)?;
-    let mut reader = BufReader::new(file);
-    let mut buffer = Vec::new();
-
-    // Read file
-    match reader.read_to_end(&mut buffer) {
-        Ok(_) => return Ok(buffer),
-        Err(e) => Err(e),
-    }
+    let file_bytes = fs::read(filepath)?;
+    Ok(file_bytes)
 }
 
 pub fn save_file_bytes(filepath: &str, file_data: Vec<u8>) -> Result<(), Error> {
     let encrypted_filepath = format!("{}.enc", filepath);
-    let mut file = fs::OpenOptions::new()
-        .write(true)
-        .open(encrypted_filepath)?;
-
-    match file.write_all(&file_data) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(e),
-    }
+    let mut file = File::create(encrypted_filepath)?;
+    file.write_all(&file_data)?;
+    Ok(())
 }
